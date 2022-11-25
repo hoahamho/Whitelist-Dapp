20aba3697c941d5bbabd94cae12ce23229e96182
hoahamho <hoaciven@gmail.com>
2022-11-25 11:41:25 +0700

    new

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,10 +1,10 @@
-export const WHITELIST_CONTRACT_ADDRESS = "0x3bCeB2Acb6A7aff69eC6D17596D997763C005a06";
+export const WHITELIST_CONTRACT_ADDRESS = "0x5407dc8e2F7dB5dE85CEC0fF6Ae301E6323295DB";
 export const abi = [
     {
         "inputs": [
             {
                 "internalType": "uint8",
-                "name": "_maxWhitelistedAddress",
+                "name": "_maxWhitelistedAddresses",
                 "type": "uint8"
             }
         ],
@@ -13,7 +13,7 @@ export const abi = [
     },
     {
         "inputs": [],
-        "name": "addAddressToWhilelist",
+        "name": "addAddressToWhitelist",
         "outputs": [],
         "stateMutability": "nonpayable",
         "type": "function"
@@ -33,7 +33,7 @@ export const abi = [
     },
     {
         "inputs": [],
-        "name": "numberAddressWhitelisted",
+        "name": "numAddressesWhitelisted",
         "outputs": [
             {
                 "internalType": "uint8",
