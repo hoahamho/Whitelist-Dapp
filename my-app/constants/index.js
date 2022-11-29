8d954fcf9591696eb856f0ed30bbcd199cce1d07
hoahamho <hoaciven@gmail.com>
2022-11-29 15:14:06 +0700

    new contract address

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,4 +1,4 @@
-export const WHITELIST_CONTRACT_ADDRESS = "0x5407dc8e2F7dB5dE85CEC0fF6Ae301E6323295DB";
+export const WHITELIST_CONTRACT_ADDRESS = "0xe51beAC93c5B36595E4569b38893329A61c74972";
 export const abi = [
     {
         "inputs": [
